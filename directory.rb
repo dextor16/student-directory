61cb8b5de81b85fe774e4d80198163b0349f9b33
dextor16 <jamestcork92@gmail.com>
2019-11-28 19:41:05 +0000

    Hobby commit

--- directory.rb
+++ directory.rb
@@ -1,39 +1,54 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-
   students = []
-
+  # Ask user for input
   name = gets.chomp
-
+  puts "Enter a hobby "
+  hobby = gets.chomp
+  puts "Where were they born?"
+  cob = gets.chomp
+  # repeat while name is empty
   while !name.empty? do
-
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-
-    name =  gets.chomp
+      #Add student has to the array
+      students << {name: name, cohort: :november, hobby: hobby, cob: cob}
+      puts "Now we have #{students.count} students"
+      name = gets.chomp
+      if name.empty?
+          break
+      end
+      puts "Enter a hobby"
+      hobby = gets.chomp
+      puts "Where were they born?"
+      cob = gets.chomp
   end
+  # return the array
   students
 end
-
 def print_header
   puts "The students of Villains Academy"
-  puts "-------------------"
+  puts "---------------"
 end
-
 def print(students)
-  students.each_with_index do |student, index|
-    if student[:name][0] == 'M'
-      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
-    end
+  x = 0
+  while x < students.count do
+      puts "#{students[x][:name]}, Hobby: #{students[x][:hobby]}, Place of birth: #{students[x][:cob]} (#{students[x][:cohort]} cohort)"
+      x = x + 1
   end
 end
-
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+# def print(students)
+#     students.each do |student|
+#         if student[:name].length < 12
+#             puts " #{student[:name]} (#{student[:cohort]} cohort)"
+#         end
+#     end
+# end
+# Print total number of students
+def print_footer(names)
+  puts "Overall, we have #{names.count} great student"
 end
-
 students = input_students
 print_header
 print(students)
 print_footer(students)
+
